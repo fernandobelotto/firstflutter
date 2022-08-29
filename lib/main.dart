ab23004d42413c5c2e3e74a70bdb964a0afbe3f3
Fernando <fernando.bbosco@gmail.com>
2022-08-29 10:31:58 -0300

    part2:done

--- lib/main.dart
+++ lib/main.dart
@@ -10,16 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'flutterapp',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Welcome to flutter!"),
-        ),
-        body: const Center(
-          child: RandomWords(),
-        ),
-      ),
+      home: RandomWords(),
     );
   }
 }
@@ -33,24 +26,75 @@ class RandomWords extends StatefulWidget {
 
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
   final _biggerFont = const TextStyle(fontSize: 18);
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: (context, i) {
-          if (i.isOdd) return const Divider();
-          final index = i ~/ 2;
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
+    void _pushSaved() {
+      Navigator.of(context).push(MaterialPageRoute<void>(builder: (context) {
+        final tiles = _saved.map((pair) {
           return ListTile(
             title: Text(
-              _suggestions[index].asPascalCase,
+              pair.asPascalCase,
               style: _biggerFont,
             ),
           );
         });
+
+        final divided = tiles.isNotEmpty
+            ? ListTile.divideTiles(tiles: tiles, context: context).toList()
+            : <Widget>[];
+
+        return Scaffold(
+          appBar: AppBar(title: const Text("saved suggestions")),
+          body: ListView(children: divided),
+        );
+      }));
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Welcome to flutter!"),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.list),
+            onPressed: _pushSaved,
+            tooltip: 'Saved Suggestions',
+          )
+        ],
+      ),
+      body: ListView.builder(
+          padding: const EdgeInsets.all(16.0),
+          itemBuilder: (context, i) {
+            if (i.isOdd) return const Divider();
+            final index = i ~/ 2;
+            if (index >= _suggestions.length) {
+              _suggestions.addAll(generateWordPairs().take(10));
+            }
+
+            final alreadySaved = _saved.contains(_suggestions[index]);
+
+            return ListTile(
+                title: Text(
+                  _suggestions[index].asPascalCase,
+                  style: _biggerFont,
+                ),
+                trailing: Icon(
+                  alreadySaved ? Icons.favorite : Icons.favorite_border,
+                  color: alreadySaved ? Colors.red : null,
+                  semanticLabel: alreadySaved ? 'Remove from saved' : 'save',
+                ),
+                onTap: () {
+                  setState(() {
+                    if (alreadySaved) {
+                      _saved.remove(_suggestions[index]);
+                    } else {
+                      _saved.add(_suggestions[index]);
+                    }
+                  });
+                });
+          }),
+    );
   }
 }
